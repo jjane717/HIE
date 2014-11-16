ba60ecd2a3b6b7f9ce72ce231c10b7d67fba348a
silenceleaf <silenceleaf@gmail.com>
2014-11-16 10:37:35 -0800

    Security

--- src/main/java/org/yijun/hie/config/SecurityConfig.java
+++ src/main/java/org/yijun/hie/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.yijun.hie.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) {
         try {
             auth
