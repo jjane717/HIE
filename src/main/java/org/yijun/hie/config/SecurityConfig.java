1ee746f3ea397197474d423bae03a1798ad5d812
silenceleaf <silenceleaf@gmail.com>
2014-11-15 20:02:21 -0500

    Security

--- src/main/java/org/yijun/hie/config/SecurityConfig.java
+++ src/main/java/org/yijun/hie/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.yijun.hie.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -21,5 +22,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin()
+                    .loginPage("/login")
+                    .failureUrl("/login?login_error=1")
+                    .defaultSuccessUrl("/home")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/index")
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/signup","/about").permitAll()
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().authenticated();
+
     }
 }
